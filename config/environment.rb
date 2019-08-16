4d79f1d39c135ef641aac61cf7a397218d70bfc6
Hans-Conradt v. Wedemeyer <hc@vonwedemeyer.com>
2019-08-16 10:52:18 +0200

    added cards

--- config/environment.rb
+++ config/environment.rb
@@ -5,15 +5,4 @@ require_relative 'application'
 Rails.application.initialize!
 
 
-const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
-
-module.exports = environment
